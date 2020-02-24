8658d16dc62cc3d23b0ff3a903272daa23c857dd
filipkastovsky <devfilipkastovsky@gmail.com>
2020-02-25 00:33:34 +0100

    Updates

---
+++ precache-manifest.87bbf9c4031153d469189c85d51d67d2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a629efdeb121dac16326",
+    "url": "/stravovaci-navyky/static/js/main.a629efde.chunk.js"
+  },
+  {
+    "revision": "dd959c275b1f11c69016",
+    "url": "/stravovaci-navyky/static/css/1.390f1275.chunk.css"
+  },
+  {
+    "revision": "dd959c275b1f11c69016",
+    "url": "/stravovaci-navyky/static/js/1.dd959c27.chunk.js"
+  },
+  {
+    "revision": "a05ae6866682f471fa9e",
+    "url": "/stravovaci-navyky/static/js/runtime~main.a05ae686.js"
+  },
+  {
+    "revision": "bbaea0b9e7029d99a08308c321926984",
+    "url": "/stravovaci-navyky/static/media/bg.bbaea0b9.jpg"
+  },
+  {
+    "revision": "a00372b76ae88cb84277be8196c0d3ee",
+    "url": "/stravovaci-navyky/static/media/burger.a00372b7.jpg"
+  },
+  {
+    "revision": "d269d38ee7104d33be94f34668078012",
+    "url": "/stravovaci-navyky/static/media/vegan.d269d38e.jpg"
+  },
+  {
+    "revision": "39be4c3e3946a84eb6639dafd7526324",
+    "url": "/stravovaci-navyky/static/media/shop.39be4c3e.jpg"
+  },
+  {
+    "revision": "cda1620475cdd447fff1a59d974141ec",
+    "url": "/stravovaci-navyky/static/media/tomato.cda16204.jpg"
+  },
+  {
+    "revision": "e8d585b6a94d71b0a6577669f7370c47",
+    "url": "/stravovaci-navyky/static/media/diet.e8d585b6.jpg"
+  },
+  {
+    "revision": "bbe5d41eb724f9ac6b8212e0aed65e45",
+    "url": "/stravovaci-navyky/static/media/meat.bbe5d41e.jpg"
+  },
+  {
+    "revision": "4f9a8f650f5c7e7b822850d6843cfae5",
+    "url": "/stravovaci-navyky/static/media/bread.4f9a8f65.jpg"
+  },
+  {
+    "revision": "03f011c9db481b57cc6e2819bb629360",
+    "url": "/stravovaci-navyky/static/media/salad.03f011c9.jpg"
+  },
+  {
+    "revision": "1b8c91c9ede9e1afe48951502191ccc3",
+    "url": "/stravovaci-navyky/index.html"
+  }
+];
